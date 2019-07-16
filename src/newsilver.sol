88238bf9909e57aa353296d13592e0fea48a3e11
Charly <charly@centrifuge.io>
2019-07-16 17:39:43 +0200

    initial commit

--- src/newsilver.sol
+++ src/newsilver.sol
@@ -20,6 +20,17 @@ import "privacy-enabled-erc721/nft.sol";
 
 
 contract NewSilverLoanNFT is NFT {
+
+    // array of field names that are being proved
+    bytes[] internal _mandatoryFields;
+
+    // compact property for 'amount'
+    bytes constant internal AMOUNT = hex"0x010000000000001cdb691b0c78e9e1432d354d52e26b3cd5054cd1261c4272bf8fce2bcf285908f300000005";
+    // compact property for 'asis_value'
+    bytes constant internal ASIS_VALUE = hex"0x010000000000001cc559f889f1afe5f0e8d3ad327b66c9b2facadb9918e2ba45963fe76e590f9e4200000005";
+    // compact property for 'rehab_value'
+    bytes constant internal REHAB_VALUE = hex"0x010000000000001cdb691b0c78e9e1432d354d52e26b3cd5054cd1261c4272bf8fce2bcf285908f300000005";
+
     struct TokenData {
         uint document_version;
         uint amount;
@@ -28,14 +39,37 @@ contract NewSilverLoanNFT is NFT {
         address borrower;
     }
     mapping (uint => TokenData) public data;
-   
+
     constructor (address anchors_) NFT("Newsilver Loan NFT", "NSLN", anchors_) public {
+      _mandatoryFields.push(AMOUNT);
+      _mandatoryFields.push(ASIS_VALUE);
+      _mandatoryFields.push(REHAB_VALUE);
     }
 
-    function mint(address usr, uint tkn, uint anchor, bytes32 data_root, bytes32 signatures_root) public {
-        data[tkn] = TokenData(anchor, 0, 0, 0, usr);
-        _mint(usr, tkn);
+    function mint(address usr, uint tkn, uint anchor, bytes32 data_root, bytes32 signatures_root bytes[] memory properties bytes32[] memory values bytes32[] memory salts, [][]bytes32 proofs) public {
+
+      bytes[] memory property_names =
+      for (uint i = 0; i < 3; i++) {
+      require(property[i] == _mandatoryFields[i], "Provided proof is not one of the mandatory fields.");
+      }
+
+      data[tkn] = TokenData(
+        anchor,
+        values[0],
+        values[1],
+        values[2],
+        usr
+      );
+
+      bytes32[] memory leaves = new bytes32[](3);
+      leafs[0] = (sha256(abi.encodePacked(properties[0], values[0], salt[0]))
+      leafs[1] = (sha256(abi.encodePacked(properties[1], values[1], salt[1]))
+      leafs[2] = (sha256(abi.encodePacked(properties[2], values[2], salt[2]))
+
+      verify(leafs, proofs, root)
+      // check anchor
+      _mint(usr, tkn);
     }
-} 
+}
 
 
