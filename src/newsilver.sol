eb72555abc2627170a17cca19d7cbbab7387c87c
Charly <charly@centrifuge.io>
2019-07-17 11:44:09 +0200

    fix

--- src/newsilver.sol
+++ src/newsilver.sol
@@ -21,11 +21,11 @@ import "privacy-enabled-erc721/nft.sol";
 
 contract NewSilverLoanNFT is NFT {
     // compact property for 'amount'
-    bytes constant internal AMOUNT = hex"0x010000000000001cdb691b0c78e9e1432d354d52e26b3cd5054cd1261c4272bf8fce2bcf285908f300000005";
+    bytes constant internal AMOUNT = hex"010000000000001cdb691b0c78e9e1432d354d52e26b3cd5054cd1261c4272bf8fce2bcf285908f300000005";
     // compact property for 'asis_value'
-    bytes constant internal ASIS_VALUE = hex"0x010000000000001cc559f889f1afe5f0e8d3ad327b66c9b2facadb9918e2ba45963fe76e590f9e4200000005";
+    bytes constant internal ASIS_VALUE = hex"010000000000001cc559f889f1afe5f0e8d3ad327b66c9b2facadb9918e2ba45963fe76e590f9e4200000005";
     // compact property for 'rehab_value'
-    bytes constant internal REHAB_VALUE = hex"0x010000000000001cdb691b0c78e9e1432d354d52e26b3cd5054cd1261c4272bf8fce2bcf285908f300000005";
+    bytes constant internal REHAB_VALUE = hex"010000000000001cdb691b0c78e9e1432d354d52e26b3cd5054cd1261c4272bf8fce2bcf285908f300000005";
 
     struct TokenData {
         uint document_version;
@@ -39,12 +39,12 @@ contract NewSilverLoanNFT is NFT {
     constructor (address anchors_) NFT("Newsilver Loan NFT", "NSLN", anchors_) public {
     }
 
-    function mint(address usr, uint tkn, uint anchor, bytes32 data_root, bytes32 signatures_root bytes[] memory properties bytes32[] memory values bytes32[] memory salts, [][]bytes32 proofs) public {
+    function mint(address usr, uint tkn, uint anchor, bytes32 data_root, bytes32 signatures_root, bytes[] memory properties, bytes32[] memory values, bytes32[] memory salts, bytes32[][] memory proofs) public {
 
 
-      require(property[0] == AMOUNT, "Provided proof is not one of the mandatory fields.");
-      require(property[1] == ASIS_VALUE, "Provided proof is not one of the mandatory fields.");
-      require(property[2] == REHAB_VALUE, "Provided proof is not one of the mandatory fields.");
+      require(properties[0] == AMOUNT, "Provided proof is not one of the mandatory fields.");
+      require(properties[1] == ASIS_VALUE, "Provided proof is not one of the mandatory fields.");
+      require(properties[2] == REHAB_VALUE, "Provided proof is not one of the mandatory fields.");
 
       data[tkn] = TokenData(
         anchor,
@@ -54,13 +54,14 @@ contract NewSilverLoanNFT is NFT {
         usr
       );
 
-      bytes32[] memory leaves = new bytes32[](3);
-      leafs[0] = (sha256(abi.encodePacked(properties[0], values[0], salt[0]))
-      leafs[1] = (sha256(abi.encodePacked(properties[1], values[1], salt[1]))
-      leafs[2] = (sha256(abi.encodePacked(properties[2], values[2], salt[2]))
 
-      verify(leafs, proofs, root)
-      _checkAnchor(anchor, data_root, signatures_root)
+      bytes32 leaf1 = sha256(abi.encodePacked(properties[0], values[0], salts[0]));
+      bytes32 leaf2 = sha256(abi.encodePacked(properties[1], values[1], salts[1]));
+      bytes32 leaf3 = sha256(abi.encodePacked(properties[2], values[2], salts[2]));
+      bytes32[3] memory leaves = [leaf1, leaf2, leaf3];
+
+      verify(leaves, proofs, data_root);
+      _checkAnchor(anchor, data_root, signatures_root);
       _mint(usr, tkn);
     }
 }
